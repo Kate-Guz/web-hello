17a641940940930f552cb5cd06b7d4cfa554dfdc
ssuprr <mdmashechka777@gmail.com>
2022-12-07 07:57:55 +0000

    removed line and added comment

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ RESOURCE_DIR = os.path.join(BASE_FOLDER, "resources")
 def hello_world():
     with open(os.path.join(RESOURCE_DIR, "response.json")) as f:
         return "%s - %s" % (json.loads(f.read()).get("payload"), datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S"))
-
-
+# my changes
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
